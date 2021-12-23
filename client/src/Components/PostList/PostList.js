09eafc4562ecb9ea7dad3fe02617c34d39526e60
Dboee <davidboee@gmail.com>
2021-12-23 13:20:02 +0100

    adds CommentCreate component

--- client/src/Components/PostList/PostList.js
+++ client/src/Components/PostList/PostList.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import CommentCreate from '../CommentCreate/CommentCreate'
 
 
 export default () => {
@@ -21,9 +22,10 @@ export default () => {
         style={{width: "30%", marginBottom: "20px"}}
         key={post.id}
         >
-        <div className='card-body'>
-        <h3>{post.title}</h3>
-        </div>
+            <div className='card-body'>
+                <h3>{post.title}</h3>
+                <CommentCreate postId={post.id} />
+            </div>
         </div>
     } );
     
