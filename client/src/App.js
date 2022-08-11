1cebbe74e5720548a29c08413b813fe652895345
Dboee <davidboee@gmail.com>
2022-08-11 12:59:23 +0200

    adds subheading on client appliaction

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import PostCreate from "./Components/PostCreate/PostCreate.js"
-import PostList from "./Components/PostList/PostList.js"
+import PostCreate from './Components/PostCreate/PostCreate.js';
+import PostList from './Components/PostList/PostList.js';
 
 const App = () => {
-    return (
-      <div className="container">
-        <h1>Create Post</h1>
-        <PostCreate />
-        <hr />
+  return (
+    <div className='container'>
+      <h1>Create Post</h1>
+      <h3>If you want to</h3>
+      <PostCreate />
+      <hr />
 
-          <h1>Posts</h1>
-          <PostList />
-
-      </div>
-    );
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
 };
 
 export default App;
